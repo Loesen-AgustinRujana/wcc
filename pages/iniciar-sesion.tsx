3c95842490f3f3b7afb91a2a4316517759b0ec7d
Agustin Rujana <87715028+Loesen-AgustinRujana@users.noreply.github.com>
2021-11-23 21:49:42 +0100

    Update iniciar-sesion.tsx

--- pages/iniciar-sesion.tsx
+++ pages/iniciar-sesion.tsx
@@ -30,7 +30,7 @@ const LogIn: NextPage = () => {
     if (accessToken !== "0") verify();
   }, []);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLInputElement>) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     const data = {
@@ -89,7 +89,7 @@ const LogIn: NextPage = () => {
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                     value={email}
-                    onChange={(e: React.FormEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                    onChange={(e: any) => setEmail(e.target.value)}
                   />
                 </div>
               </div>
@@ -110,7 +110,7 @@ const LogIn: NextPage = () => {
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                     value={password}
-                    onChange={(e: React.FormEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                    onChange={(e: any) => setPassword(e.target.value)}
                   />
                 </div>
               </div>
